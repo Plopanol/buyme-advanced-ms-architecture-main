5f7a974002e1facd7cc494edfee5eb5217200b48
Plopanol <inolivierwetrust@protonmail.ch>
2025-07-03 12:01:32 +0200

    Autorization Service : Ajout de la secu

--- authorization-service/src/main/java/com/improvemyskills/security/SecurityConfig.java
+++ authorization-service/src/main/java/com/improvemyskills/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.improvemyskills.customerservice.security;
+package com.improvemyskills.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
